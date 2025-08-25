e81066d471805ac90a257d0f204b256820ebff63
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2025-08-24 21:17:51 -0300

    feat: criando routes e controllers

---
+++ src/controllers/PessoaController.js
@@ -0,0 +1,11 @@
+class PessoaController {
+    static async pegaTodas (req, res){
+        try {
+            // acessar modelo
+        } catch (err) { 
+            //erro
+        }
+    }
+}
+
+module.exports = PessoaController;
