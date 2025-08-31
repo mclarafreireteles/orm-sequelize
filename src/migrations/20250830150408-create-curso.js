7e06cf54febce67c72243664680f8720d5fb3d2d
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2025-08-31 18:11:04 -0300

    adiciona modelos

--- src/migrations/20250830150408-create-curso.js
+++ src/migrations/20250830150408-create-curso.js
@@ -2,27 +2,21 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('pessoas', {
+    await queryInterface.createTable('cursos', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      nome: {
+      titulo: {
         type: Sequelize.STRING
       },
-      email: {
+      descricao: {
         type: Sequelize.STRING
       },
-      cpf: {
-        type: Sequelize.STRING
-      },
-      ativo: {
-        type: Sequelize.BOOLEAN
-      },
-      role: {
-        type: Sequelize.STRING
+      data_inicio: {
+        type: Sequelize.DATEONLY
       },
       createdAt: {
         allowNull: false,
@@ -35,6 +29,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('pessoas');
+    await queryInterface.dropTable('cursos');
   }
 };
