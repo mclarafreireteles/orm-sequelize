c442d00436fac0970bdea0e753675f2d0ee87179
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2025-09-01 10:16:58 -0300

    adiciona services e controllers

--- src/seeders/20250901120310-demo-categorias.js
+++ src/seeders/20250901120310-demo-categorias.js
@@ -3,23 +3,31 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('categorias', [
+      {
+        titulo: 'Node.js',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        titulo: 'Java',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        titulo: 'Python',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        titulo: 'C#',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('categorias', null, {});
   }
 };
